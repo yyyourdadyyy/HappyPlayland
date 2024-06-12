0c25921a6cb2a13a30c0fde78ca2f02305fe05cf
gub <githubmaavel@gmail.com>
2024-06-12 14:37:33 +0300

    Новая система управления для телефонов, готовая страница мультиков для телефонов

--- js/app.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    menu.classList.toggle("active-burger");
-  
-    var bars = document.querySelectorAll(".bar");
-    bars.forEach(function(bar) {
-      bar.classList.toggle("active-burgerR");
-    });
-  }
